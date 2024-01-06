e3966987dcb1965674587d155fd8645f65925650
Diego <miguelanez.diego@gmail.com>
2024-01-06 19:51:40 +0100

    Automated commit by bash script

--- git-push.sh
+++ git-push.sh
@@ -2,4 +2,4 @@
 
 git add .
 git commit . -m "Automated commit by bash script"
-git push https://DiegoMiguelanez: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@github.com/DiegoMiguelanez/python.git main
+git push https://DiegoMiguelanez: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@github.com/DiegoMiguelanez/python.git main
